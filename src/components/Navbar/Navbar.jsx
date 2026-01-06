5460be895dc288f892196b0dcbdb43e9cee68f89
Sanjay Malviya <sanjaymalviyaaa8@gmail.com>
2026-01-07 03:35:43 +0530

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { FiMenu, FiX } from 'react-icons/fi';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { navLinks } from '../../constants';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [activeSection, setActiveSection] = useState('');
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const handleMenuItemClick = (id) => {
+        setActiveSection(id);
+        setIsOpen(false);
+
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            setIsScrolled(scrollPosition > 50);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <nav className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7v] lg:[20vw] ${isScrolled ? 'bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md0' : 'bg-transparent'}`}>
+            <div className='text-white py-5 flex justify-between items-center'>
+                {/* Logo */}
+                <div className='text-white py-5 flex justify-between items-center'>
+                    <span className='text-[#8245ec]'>&lt;</span>
+                    <span className='text-white'>Sanjay</span>
+                    <span className='text-[#8245ec]'>/</span>
+                    <span className='text-white'>Malviya</span>
+                    <span className='text-[#8245ec]'>&gt;</span>
+                </div>
+
+                {/* Desktop Navigation */}
+                <ul className='hidden md:flex space-x-8 text-gray-300'>
+                    {navLinks.map((item) => (
+                        <li key={item.id} className={`cursor-pointer hover:text-[#8245ec] ${activeSection === item.id ? 'text-[#8245ec]' : ''}`}>
+                            <button onClick={() => handleMenuItemClick(item.id)}>{item.label}</button>
+                        </li>
+                    ))}
+                </ul>
+
+                {/* Social Media Icons */}
+                <div className='hidden md:flex space-x-4'>
+                    <a
+                        href="https://github.com/sanjaymalviyaaa8"
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='text-gray-300 hover:text-[#8245ec]'
+                    >
+                        <FaGithub size={24} />
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/sanjaymalviya"
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='text-gray-300 hover:text-[#8245ec]'
+                    >
+                        <FaLinkedin size={24} />
+                    </a>
+                </div>
+
+                {/* Mobile Menu Icons */}
+                <div className='md:hidden'>
+                    {
+                        isOpen ? (
+                            <FiX size={28} className='text-3xl text-[#8245ec] cursor-pointer' onClick={() => setIsOpen(false)} />
+                        ) : (
+                            <FiMenu size={28} className='text-3xl text-[#8245ec] cursor-pointer' onClick={() => setIsOpen(true)} />
+                        )
+                    }
+                </div>
+            </div>
+
+            {/* Mobile Navigation Menu */}
+            {isOpen && (
+                <div className="absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden">
+                    <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+                        {navLinks.map((item) => (
+                            <li
+                                key={item.id}
+                                className={`cursor-pointer hover:text-white ${activeSection === item.id ? "text-[#8245ec]" : ""
+                                    }`}
+                            >
+                                <button onClick={() => handleMenuItemClick(item.id)}>
+                                    {item.label}
+                                </button>
+                            </li>
+                        ))}
+                        <div className="flex space-x-4">
+                            <a
+                                href="https://github.com/sanjaymalviyaaa8"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-gray-300 hover:text-white"
+                            >
+                                <FaGithub size={24} />
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/sanjaymalviya"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-gray-300 hover:text-white"
+                            >
+                                <FaLinkedin size={24} />
+                            </a>
+                        </div>
+                    </ul>
+                </div>
+            )}
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/constants.js
@@ -0,0 +1,250 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import LaravelLogo from './assets/tech_logo/laravel.png';
+import PHPLogo from './assets/tech_logo/php.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+import profilicsLogo from './assets/company_logo/profilics.png';
+import ApliteLogo from './assets/company_logo/aplite_info_solution.png';
+import indvibeLogo from './assets/company_logo/indvibe_infotech_pvt_ltd_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import ICSELogo from './assets/education_logo/icse.png'
+import MPBoardLogo from './assets/education_logo/mpboard.png'
+import RGPVLogo from './assets/education_logo/rgpv.png'
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import leankorLogo from './assets/work_logo/leankor.png';
+import aesthetidocsLogo from './assets/work_logo/aesthetidocs.png';
+
+
+export const SkillsInfo = [
+    {
+        title: 'Frontend',
+        skills: [
+            { name: 'HTML', logo: htmlLogo },
+            { name: 'CSS', logo: cssLogo },
+            { name: 'SASS', logo: sassLogo },
+            { name: 'JavaScript', logo: javascriptLogo },
+            { name: 'React JS', logo: reactjsLogo },
+            { name: 'Redux', logo: reduxLogo },
+            { name: 'Next JS', logo: nextjsLogo },
+            { name: 'Tailwind CSS', logo: tailwindcssLogo },
+            { name: 'Material UI', logo: materialuiLogo },
+            { name: 'Bootstrap', logo: bootstrapLogo },
+        ],
+    },
+    {
+        title: 'Backend',
+        skills: [
+            { name: 'Laravel', logo: LaravelLogo },
+            // { name: 'Node JS', logo: nodejsLogo },
+            // { name: 'Express JS', logo: expressjsLogo },
+            // { name: 'Firebase', logo: firebaseLogo },
+            { name: 'PostgreSQL', logo: postgreLogo },
+        ],
+    },
+    {
+        title: 'Languages',
+        skills: [
+            { name: 'JavaScript', logo: javascriptLogo },
+            { name: 'PHP', logo: PHPLogo },
+        ],
+    },
+    {
+        title: 'Tools',
+        skills: [
+            { name: 'Git', logo: gitLogo },
+            { name: 'GitHub', logo: githubLogo },
+            { name: 'VS Code', logo: vscodeLogo },
+            { name: 'Postman', logo: postmanLogo },
+            // { name: 'Compass', logo: mcLogo },
+            { name: 'Vercel', logo: vercelLogo },
+            { name: 'Figma', logo: figmaLogo },
+        ],
+    },
+];
+
+export const experiences = [
+    {
+        id: 0,
+        img: profilicsLogo,
+        role: "Fullstack Developer",
+        company: "PROFILICS SYSTEMS Pvt. Ltd.",
+        date: "August 2024 - Present",
+        desc: "Currently working at Profilics Systems Pvt. Ltd. as a React + Laravel Developer, building scalable and secure full-stack applications. Responsible for developing modern front-end interfaces using React, integrating them with Laravel APIs, and managing applications using Docker and PostgreSQL. Actively involved in authentication systems, role-based access control, and delivering production-ready features.",
+        skills: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React JS",
+            "Laravel",
+            "Tailwind CSS",
+            "Redux",
+            " Next Js",
+            "Figma",
+            "Material UI",
+        ],
+    },
+    {
+        id: 1,
+        img: ApliteLogo,
+        role: "UI Developer",
+        company: "Aplite Info Solution",
+        date: "March 2023 - July 2024",
+        desc: "Worked as a JavaScript Developer at Aplite Info Solution, contributing to the development of responsive and user-friendly web applications. Gained hands-on experience with React.js, core JavaScript, and REST API integration while working on real-world projects like Leankor, a calendar-based application developed using Agile methodology. Focused on improving UI performance, fixing bugs, and collaborating with cross-functional teams.",
+        skills: [
+            "JavaScript",
+            "SASS",
+            "HTML",
+            "CSS",
+            "SQL",
+            "ExtJS",
+            "Apex(LWC)",
+        ],
+    },
+    {
+        id: 2,
+        img: indvibeLogo,
+        role: "Frontend Intern (ReactJS)",
+        company: "Indvibe Infotech Pvt. Ltd.",
+        date: "April 2022 - August 2022",
+        desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+        skills: [
+            "React JS",
+            "HTML",
+            "CSS",
+            "Javascript",
+            "Bootstrap",
+            "Figma",
+            "Material UI",
+        ],
+    },
+];
+
+export const education = [
+    {
+        id: 0,
+        img: RGPVLogo,
+        school: "Malwa Institute of Science and Technology",
+        date: "Aug 2022 - Present",
+        grade: "",
+        desc: "Currently pursuing B.Tech in Information Technology, where I am building a strong foundation in software development and core IT subjects. My studies focus on programming, web technologies, data structures, and database systems, along with hands-on project work that strengthens practical problem-solving and development skills.",
+        degree: "Bachelor of Technology (B.Tech) – Information Technology",
+    },
+    {
+        id: 1,
+        img: MPBoardLogo,
+        school: "Govt. Excellence H. S. School, Ujjain",
+        date: "Apr 2021 - March 2022",
+        grade: "",
+        desc: "I completed my class 12 education from Govt. Excellence H. S. School, Ujjain, under the MP board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+        degree: "MP Board(XII) - PCM with Computer Science",
+    },
+    {
+        id: 2,
+        img: RGPVLogo,
+        school: "Government Polytechnic College, Ujjain",
+        date: "Aug 2015 - March 2020",
+        grade: "",
+        desc: "I completed my polytechnic from Government Polytechnic College, Ujjain, under RGPV Bhopal.",
+        degree: "Computer Science Engineering - Diploma",
+    },
+    {
+        id: 3,
+        img: ICSELogo,
+        school: "Mahatma Gandhi Convent Higher Secondary School, Shajapur",
+        date: "Apr 2014 - March 2015",
+        grade: "",
+        desc: "I completed my class 10 education from Mahatma Gandhi Convent Higher Secondary School, Shajapur, under the ICSE board, where I studied Science with Computer.",
+        degree: "ICSE(X), Science with Computer Application",
+    },
+];
+
+export const projects = [
+
+    {
+        id: 0,
+        title: "Leankor",
+        description:
+            "LeanKor is an enterprise project and work management platform designed to manage tasks, timelines, and team collaboration. I worked on building and enhancing React-based UI components, integrating APIs, and improving the overall user experience for a calendar-driven project management system.",
+        image: leankorLogo,
+        tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+        webapp: "https://www.leankor.com/",
+    },
+    {
+        id: 1,
+        title: "Aesthetidocs",
+        description:
+            "AesthetiDocs is a clinic management and appointment booking platform for aesthetic practices. I contributed to developing responsive React interfaces, integrating Laravel APIs, and implementing features related to appointments, documentation, and role-based access.",
+        image: aesthetidocsLogo,
+        tags: ["HTML", "CSS", "JavaScript", "React JS", "API", "Laravel", "Tailwind CSS", "MUI", "PostgreSQL"],
+        webapp: "https://www.aesthetidocs.com/",
+    },
+    {
+        id: 3,
+        title: "Personal Portfolio Website",
+        description:
+            "Personal portfolio website showcasing my experience, projects, and skills as a React developer. Built with a focus on performance, responsive design, and clean UI.",
+        image: aesthetidocsLogo,
+        tags: ["HTML", "CSS", "JavaScript", "React JS", "Tailwind CSS"],
+        github: "https://github.com/sanjaymalviyaaa8",
+        webapp: "https://www.aesthetidocs.com/",
+    },
+];
+
+export const navLinks = [
+    { id: 'about', label: 'About' },
+    { id: 'skills', label: 'Skills' },
+    { id: 'experience', label: 'Experience' },
+    { id: 'work', label: 'Projects' },
+    { id: 'education', label: 'Education' },
+    { id: 'contact', label: 'Contact' },
+];
